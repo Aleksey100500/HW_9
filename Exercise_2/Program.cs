051b44e5dcc0b867a718061ab1f2d4d2f084914e
Aleksey <a_zhuravlyov@bk.ru>
2022-09-06 20:39:16 +0300

    Finished Exercise_2

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -2,3 +2,22 @@
 
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30*/
+
+Console.Write("Задайте число M - ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте число N - ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void SumNumbers()
+{
+    int Sum = 0;
+    
+    Console.Write($"M = {M}; N = {N}. ->");
+    while (M <= N)
+    {
+        Sum += M++;
+    }
+    Console.Write($" {Sum}");
+}
+SumNumbers();
